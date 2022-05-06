bc07eed072ca4c8165200eafb97dfcf2dffea8ca
Ivan <kubrak.ivan@gmail.com>
2022-05-06 10:48:11 +0300

    for and while practice

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
 "use strict";
 
-// if (4 == 9) {
-//     console.log('Ok!');
-// } else {
-//     console.log('Error!');
-// };
 
-// Тернарный оператор
+// for (let i = 0; i < 3; i++) {
+//         console.log(i);
+//         for (let j = 0; j < 3; j++) {
+//             console.log(j);
+            
+//     }
+// }
 
-// const num = 50;
+// let result = '';
+// const length = 7;
 
-// (num === 50) ? console.log('Ok!') : console.log('Error!');
+// for (let i = 1; i < length; i++) {
+//     for (let j = 0; j < i; j++) {
+//         result += '*';
+//     }
+//     result += '\n';
+// }
+// console.log(result);
 
-// Switch
-
-const num = 50;
-
-switch (num) {
-    case 49: 
-        console.log('Неверно!');
-        break;
-    case 100:
-        console.log('Неверно');
-        break;
-    case 51:
-        console.log('В точку!');
-        break;
-    default:
-        console.log('Не в этот раз!');
-        break;
+first: for (let i = 0; i < 3; i++) {
+    console.log(`First level: ${i}`);
+    for (let j = 0; j < 3; j++) {
+        console.log(`Second level: ${j}`);
+        for (let k = 0; k < 3; k++) {
+            if (k === 2) continue first;
+            console.log(`Third level: ${k}`);
+        }
+    }
 }
