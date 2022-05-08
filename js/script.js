61950c8477ecaa5d593308d119a279717655be3f
Ivan <kubrak.ivan@gmail.com>
2022-05-09 02:28:51 +0300

    third practical lesson

--- js/script.js
+++ js/script.js
@@ -9,7 +9,6 @@ P.S. Функции вызывать не обязательно */
 
 'use strict';
 
-// Код возьмите из предыдущего домашнего задания
 let numberOfFilms;
 
 function start() {
@@ -32,7 +31,7 @@ const personalMovieDB = {
 
 function rememberMyFilms() {
     for (let i = 0; i < 2; i++) {
-        const a = prompt("Один из последних просмотренных фильмов?", ""),
+        const a = prompt("Один из последних просмотренных фильмов?", "").trim(),
               b = prompt("На сколько оцените его?", "");
         
         if (a != null && b != null && a != '' && b != '' && a.length < 50) {
@@ -45,7 +44,7 @@ function rememberMyFilms() {
     }
 }
 
-// rememberMyFilms();
+rememberMyFilms();
 
 function detectPersonalLevel() {
     if (personalMovieDB.count < 10) {
@@ -59,7 +58,7 @@ function detectPersonalLevel() {
     }
 }
 
-// detectPersonalLevel();
+detectPersonalLevel();
 
 function showMyDB(hidden) {
     if (!hidden) {
@@ -76,12 +75,3 @@ function writeYourGenres() {
 }
 
 writeYourGenres();
-
-/* Задание на урок:
-1) Первую часть задания повторить по уроку
-2) Создать функцию showMyDB, которая будет проверять свойство privat. Если стоит в позиции
-false - выводит в консоль главный объект программы
-3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос 
-"Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных
-genres
-P.S. Функции вызывать не обязательно */
