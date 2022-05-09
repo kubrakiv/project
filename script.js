f5fe7c8c6994ab9d94c9395d8e59ac98f04f7546
Ivan <kubrak.ivan@gmail.com>
2022-05-10 01:53:25 +0300

    Копирование объектов и spread оператор

--- script.js
+++ script.js
@@ -1,32 +1,98 @@
 "use strict";
 
-const arr = [2, 13, 26, 8, 10];
+// let a = 5,
+//     b = a;
 
-arr.sort(compareNum);
-console.log(arr);
+// b = b + 5;
 
-function compareNum(a, b) {
-    return a - b;
+// console.log(b);
+// console.log(a);
+
+// const obj = {
+//     a: 5,
+//     b: 1
+// };
+
+// const copy = obj;
+
+// copy.a = 10;
+
+// console.log(copy);
+// console.log(obj);
+
+function copy(mainObj) {
+    let objCopy = {};
+
+    let key;
+    for (key in mainObj) {
+        objCopy[key] = mainObj[key];
+    }
+    return objCopy;
 }
 
-// arr.forEach(function(item, i, arr) {
-//     console.log(`${i}: ${item} внутри массива ${arr}`);
-// });
+const numbers = {
+    a: 2,
+    b: 5,
+    c: {
+        x: 7,
+        y: 4
+    }
+};
 
-// // arr.pop();
-// // arr.push(10);
+const newNumbers = copy(numbers);
 
-// console.log(arr);
+newNumbers.a = 10;
+newNumbers.c.x = 10;
 
-// for (let i = 0; i < arr.length; i++) {
-//     console.log(arr[i]);
-// }
+// console.log(newNumbers);
+// console.log(numbers);
 
-// for (let value of arr) {
-//     console.log(value);
-// }
+const add = {
+    d: 17,
+    e: 20
+};
 
-const str = 'соль, сахар, мука, мед, банан, абрикос';
-const products = str.split(", ");
-products.sort();
-console.log(products.join("; "));
+const clone = Object.assign({}, add);
+
+clone.d = 20;
+
+// console.log(add);
+// console.log(clone);
+
+const oldArray = ['a', 'b', 'c'];
+const newArray = oldArray.slice();
+
+newArray[1] = 'asdfasdf';
+console.log(newArray);
+console.log(oldArray);
+
+const video = ['youtube', 'vimeo', 'rutube'],
+      blogs = ['wordpress', 'livejournal', 'blogger'],
+      internet = [...video, ...blogs, 'vk', 'facebook'];
+
+console.log(internet);
+
+function log(a, b, c) {
+    console.log(a);
+    console.log(b);
+    console.log(c);
+}
+
+const num = [2, 5, 7];
+
+log(...num);
+
+const array = ['a', 'b'];
+
+const newAarray = [...array];
+
+const q = {
+    one: 1,
+    two: 2,
+    three: 3
+};
+
+const newObj = {...q};
+
+console.log(q);
+console.log(newObj);
