ab1014af244381c5354fbfb8e5daddde9dff1ea9
Ivan <kubrak.ivan@gmail.com>
2022-05-09 03:26:27 +0300

    32 objects

--- script.js
+++ script.js
@@ -78,10 +78,61 @@ const logg = "Hello world";
 
 // console.log(logg.substring(6, 11));
 
-console.log(logg.substr(6, 3));
+// console.log(logg.substr(6, 3));
 
 let guestList = "Guests:\n * John\n * Pete\n * Mary";
 
 console.log(guestList); // список гостей, состоящий из нескольких строк
 
 console.log( "\u{1F60D}" );
+
+function learnJS(lang, callback) {
+    console.log(`Я учу: ${lang}`);
+    callback();
+}
+
+function done() {
+    console.log('Я прошел этот урок');
+}
+
+learnJS('JavaScript', done);
+
+const options = {
+    name: 'test',
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: 'black',
+        bg: 'red'
+    },
+    makeTest: function() {
+        console.log('Test');
+    }
+};
+
+options.makeTest();
+
+const {border, bg} = options.colors;
+console.log(border);
+
+// console.log(Object.keys(options).length);
+
+// console.log(options["colors"]["border"]);
+
+// delete options.name;
+
+// console.log(options);
+
+// let counter = 0;
+// for (let key in options) {
+//     if (typeof(options[key]) === "object") {
+//         for (let i in options[key]) {
+//             console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+//             counter++;
+//         }
+//     } else {
+//         console.log(`Свойство ${key} имеет значение ${options[key]}`);
+//         counter++;
+//     }
+// }
+// console.log(counter);
