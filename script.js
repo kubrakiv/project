dfbe32bf28cda6c36e8078343487e5bf10221170
Ivan <kubrak.ivan@gmail.com>
2022-05-11 02:20:16 +0300

    36-37 lessons

--- script.js
+++ script.js
@@ -1,98 +1,19 @@
 "use strict";
 
-// let a = 5,
-//     b = a;
+const soldier = {
+    health: 400,
+    armour: 100,
+    sayHello: function() {
+        console.log('Hello');
+    }
+};
 
-// b = b + 5;
+const jonh = Object.create(soldier);
 
-// console.log(b);
-// console.log(a);
-
-// const obj = {
-//     a: 5,
-//     b: 1
+// const jonh = {
+//     health: 100
 // };
 
-// const copy = obj;
+// Object.setPrototypeOf(jonh, soldier);
 
-// copy.a = 10;
-
-// console.log(copy);
-// console.log(obj);
-
-function copy(mainObj) {
-    let objCopy = {};
-
-    let key;
-    for (key in mainObj) {
-        objCopy[key] = mainObj[key];
-    }
-    return objCopy;
-}
-
-const numbers = {
-    a: 2,
-    b: 5,
-    c: {
-        x: 7,
-        y: 4
-    }
-};
-
-const newNumbers = copy(numbers);
-
-newNumbers.a = 10;
-newNumbers.c.x = 10;
-
-// console.log(newNumbers);
-// console.log(numbers);
-
-const add = {
-    d: 17,
-    e: 20
-};
-
-const clone = Object.assign({}, add);
-
-clone.d = 20;
-
-// console.log(add);
-// console.log(clone);
-
-const oldArray = ['a', 'b', 'c'];
-const newArray = oldArray.slice();
-
-newArray[1] = 'asdfasdf';
-console.log(newArray);
-console.log(oldArray);
-
-const video = ['youtube', 'vimeo', 'rutube'],
-      blogs = ['wordpress', 'livejournal', 'blogger'],
-      internet = [...video, ...blogs, 'vk', 'facebook'];
-
-console.log(internet);
-
-function log(a, b, c) {
-    console.log(a);
-    console.log(b);
-    console.log(c);
-}
-
-const num = [2, 5, 7];
-
-log(...num);
-
-const array = ['a', 'b'];
-
-const newAarray = [...array];
-
-const q = {
-    one: 1,
-    two: 2,
-    three: 3
-};
-
-const newObj = {...q};
-
-console.log(q);
-console.log(newObj);
+jonh.sayHello();
